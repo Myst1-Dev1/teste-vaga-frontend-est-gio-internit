8105bf2f13e84096700f3854d2ae98997f408668
João Victor Vasconcelos Flôr <danteopzz@hotmail.com>
2024-01-17 14:50:11 -0300

    scroll has been added to enterprise

--- src/components/Enterprise/index.tsx
+++ src/components/Enterprise/index.tsx
@@ -1,6 +1,6 @@
 import './styles.scss';
 
-import { useState } from 'react';
+import { MouseEvent, useRef, useState } from 'react';
 
 import arrow1 from '../../assets/images/arrow1.png';
 import enterpriseImage1 from '../../assets/images/enterpriseImage1.png';
@@ -14,6 +14,34 @@ import yellowFrame from '../../assets/images/yellowFrame.png';
 
 export function Enterprise() {
     const [enterPriseImage, setEnterPriseImage] = useState(enterpriseImage1);
+    const [scrollValue, setScrollValue] = useState(0);
+
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+
+    const containerRef:null | any = useRef(null);
+
+    function handleMouseDown(e:MouseEvent) {
+        setIsDragging(true);
+        setStartX(e.clientX);
+    }
+
+    function handleMouseUp() {
+        setIsDragging(false);
+    }
+
+    function handleMouseMove(e:MouseEvent) {
+        if(isDragging) {
+            const deltaX = e.clientX - startX;
+            containerRef.current.scrollLeft -= deltaX * 10;
+            setStartX(e.clientX);
+        }
+    }
+
+    function handleInputChange (e:any) {
+        const value = parseInt(e.target.value, 10);
+        setScrollValue(value);
+    };
 
     return (
         <div className='enterprise' id='enterprise'>
@@ -28,15 +56,26 @@ export function Enterprise() {
                     odio malesuada ullamcorper est sed. Aliquet lacus, platea nibh ultricies turpis diam leo
                     duis. Amet massa, sit rhoncus leo laoreet habitant. Aliquam nullam risus duis curabitur.
                 </p>
-                <div className='imgContainer'>
-                    <img onClick={() => setEnterPriseImage(enterPriseImage === enterpriseImage2 ? enterpriseImage1 : enterpriseImage2)} src={enterPriseImage === enterpriseImage2 ? enterpriseImage6 : enterpriseImage3} alt="segunda imagem do emprendedor" />
-                    <img src={enterpriseImage4} alt="terceira imagem do emprendedor" />
-                    <img src={enterpriseImage5} alt="quinta imagem do emprendedor" />
+                <div 
+                    ref={containerRef}
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}
+                    onMouseMove={handleMouseMove}
+                    className='imgContainer'
+                >
+                    <img style={{ transform: `translateX(-${scrollValue}%)` }} onClick={() => setEnterPriseImage(enterPriseImage === enterpriseImage2 ? enterpriseImage1 : enterpriseImage2)} src={enterPriseImage === enterpriseImage2 ? enterpriseImage6 : enterpriseImage3} alt="segunda imagem do emprendedor" />
+                    <img style={{ transform: `translateX(-${scrollValue}%)` }} src={enterpriseImage4} alt="terceira imagem do emprendedor" />
+                    <img style={{ transform: `translateX(-${scrollValue}%)` }} src={enterpriseImage5} alt="quinta imagem do emprendedor" />
+                    <img style={{ transform: `translateX(-${scrollValue}%)` }} src={enterpriseImage6} alt="quinta imagem do emprendedor" />
+                    <img style={{ transform: `translateX(-${scrollValue}%)` }} src={enterpriseImage4} alt="terceira imagem do emprendedor" />
+                    <img style={{ transform: `translateX(-${scrollValue}%)` }} src={enterpriseImage5} alt="quinta imagem do emprendedor" />
                 </div>
                 <div className='dragDiv'>
-                    <div className='lineBox'>
-                        <div className='yellowBall'></div>
-                        <hr />
+                    <div className='rangeBox'>
+                       <input min="0"
+                            max="350"
+                            value={scrollValue}
+                            onChange={handleInputChange} type="range" />
                     </div>
                     <span>(Click e arraste)</span>
                 </div>
